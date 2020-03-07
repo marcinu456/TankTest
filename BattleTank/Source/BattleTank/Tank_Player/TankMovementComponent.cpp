c58ec0986802fb553d74c17d6f0d36b9419fad57
Marcin Pietrzak <marcinu456@gmail.com>
2020-03-07 19:13:56 +0100

    Completing Manual Tank Movement

--- BattleTank/Source/BattleTank/Tank_Player/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Tank_Player/TankMovementComponent.cpp
@@ -6,14 +6,22 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT(" Indend move throw: %f"), Throw);
+	//UE_LOG(LogTemp, Warning, TEXT(" Indend move throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	//UE_LOG(LogTemp, Warning, TEXT(" Indend move right throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
