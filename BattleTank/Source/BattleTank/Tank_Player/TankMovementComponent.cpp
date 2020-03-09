dd86813a05a86cbaf0b62e55617130f8c3cfad18
Marcin Pietrzak <marcinu456@gmail.com>
2020-03-09 16:29:19 +0100

    DotProduct() Vector Operator

--- BattleTank/Source/BattleTank/Tank_Player/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Tank_Player/TankMovementComponent.cpp
@@ -13,9 +13,12 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString);
+	//auto TankName = GetOwner()->GetName();
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardDirection = MoveVelocity.GetSafeNormal();// .ToString();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardDirection);
+	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *AIForwardDirection);
+	IntendMoveForward(ForwardThrow);
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
