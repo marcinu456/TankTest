44f52bebef2c0472ae6c2943154eeca49600f2b3
Marcin Pietrzak <marcinu456@gmail.com>
2020-03-19 22:16:56 +0100

    repair

--- BattleTank/Source/BattleTank/Tank_Player/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Tank_Player/TankTrack.cpp
@@ -1,61 +1,41 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "BattleTank.h"
 #include "TankTrack.h"
 
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay()
 {
-	Super::BeginPlay();
 	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
-<<<<<<< HEAD
 	DriveTrack();
 	ApplySidewaysForce();
 	CurrentThrottle = 0;
 }
 
 void UTankTrack::ApplySidewaysForce()
-=======
-	UE_LOG(LogTemp, Warning, TEXT("hit"));
-}
-
-
-void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
->>>>>>> master
 {
-	//Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// Work-out the required acceleration this frame to correct
 	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
-	//Work-out the required acceleration this frame to correct
-<<<<<<< HEAD
 	auto DeltaTime = GetWorld()->GetDeltaSeconds();
-=======
->>>>>>> master
 	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
-	//calculate and apply sideways (F=ma)
-	auto TankRoot = Cast<UMeshComponent>(GetOwner()->GetRootComponent());
-	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; //2 Tracks
+
+	// Calculate and apply sideways (F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks
 	TankRoot->AddForce(CorrectionForce);
 }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> master
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	//auto Name = GetName();
-	//UE_LOG(LogTemp, Warning, TEXT(" %s Throttle %f"), *Name, Throttle);
-
 	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
 }
 
--- BattleTank/Source/BattleTank/Tank_Player/TankTrack.h
+++ BattleTank/Source/BattleTank/Tank_Player/TankTrack.h
@@ -1,41 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
 #include "TankTrack.generated.h"
 
 /**
- * TankTrack is used to set maximum driving force, and apply forces to tank.
+ * TankTrack is used to set maximum driving force, and to apply forces to the tank.
  */
 UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-private:
-    UTankTrack();
-    virtual void BeginPlay() override;
-<<<<<<< HEAD
-
-    void ApplySidewaysForce();
-=======
-    virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
->>>>>>> master
-
-    UFUNCTION()
-    void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
 public:
-    //Set a throttle between -1 and +1
-    UFUNCTION(BlueprintCallable, Category = Input)
-        void SetThrottle(float Throttle);
-    
-    void DriveTrack();
+	// Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = "Input")
+		void SetThrottle(float Throttle);
 
-    float CurrentThrottle = 0;
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
 
-    //Max force per tracks in Newtons
-    UPROPERTY(EditDefaultsOnly, Category = Setup)
-        float TrackMaxDrivingForce = 400000;// Assume 40 tonne tank, and 1g acceleration
+private:
+	UTankTrack();
+
+	virtual void BeginPlay() override;
+
+	void ApplySidewaysForce();
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	void DriveTrack();
+
+	float CurrentThrottle = 0;
 };
